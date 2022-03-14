51ec2a15ea299bd0d04ac991c52ee6fb1f5b246f
ShmakovVladimir <kaktusation@gmil.com>
2022-03-15 02:28:15 +0300

    Добавление функции для рассчета коэффициентов наклона графиков

--- Термодинамика/Диффузия/plt1.py
+++ Термодинамика/Диффузия/plt1.py
@@ -12,12 +12,20 @@ def readData(path :str):
         time.append(float(line['t (s)']))
         voltage.append(float(line['V (mV)'])/(10**3))
     return time,voltage
+def count(voltage: list,time: list)->tuple:
+    voltage = [math.log(i) for i in voltage]
+    return lf.MNK(time,voltage)
 colors = ['#ff9302','#ffb701','#5586a6','#3d5a68','#212b2d']
 for i in range(5):
     t,v = readData(str(i+1)+".csv")
     voltage,time = v,t
     v.pop(0)
     while t.pop(0)<5:   v.pop(0)
+    a,b = count(voltage,time)
+    print()
+    print("Измерение: "+str(i+1))
+    a.print("Коэффициент a")
+    b.print("Коэффициент b")
     plt.yscale('log')
     plt.errorbar(x = t,
                  y = v,
