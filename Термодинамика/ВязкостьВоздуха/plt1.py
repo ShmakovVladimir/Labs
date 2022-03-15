07b4ed516bc9ee41fad3b66c805a43e80aa0c702
ShmakovVladimir <kaktusation@gmil.com>
2022-03-15 20:11:03 +0300

    Рассчитано Re

--- Термодинамика/ВязкостьВоздуха/plt1.py
+++ Термодинамика/ВязкостьВоздуха/plt1.py
@@ -24,10 +24,16 @@ def viscosityCount(QbyP: lf.value,d: lf.value,l: lf.value)->lf.value:
     visErrorPbyQ = abs(PbyQ.error*vis/PbyQ.value)
     visErrorL = abs(l.error*vis/l.value)
     return lf.value(vis,visErrord+visErrorPbyQ+visErrorL)
+def reCount(vis: lf.value,diam: lf.value):
+    Reinolds = 1.275*0.2*diam.value/(2*vis.value)
+    ReVis = abs(vis.error*Reinolds/vis.value)
+    ReDiam = abs(diam.error*Reinolds/diam.value)
+    return lf.value(Reinolds,ReVis+ReDiam/2)
 l,d,p,q = readData('data1.txt')
 a,b = lf.MNK([p[3],p[3],p[4]],[q[3],q[3],q[4]])
 aStart,bStart = lf.MNK([p[0],p[1],p[2]],[q[0],q[1],q[2]])
 etta = viscosityCount(a,d,l)
+Rein = reCount(etta,d)
 f = interp1d(x=[i.value for i in p],y=[i.value for i in q],kind = 'quadratic')
 xInter = [i/1000 for i in range(math.floor(p[0].value*1000+1),math.ceil(p[-1].value*1000-1))]
 yInter = [f(x) for x in xInter]
@@ -35,6 +41,8 @@ a.print("a coefficient")
 b.print("b coefficient")
 print()
 etta.print("air viscosity")
+print()
+Rein.print("Reinolds number")
 print()
 xT = [i for i in range(40,265,5)]
 yT = [i/100000 for i in range(10,80)]
