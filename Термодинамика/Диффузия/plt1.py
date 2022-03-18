042d0a5df008f96ea87f84b714c3774c3cc58a53
ShmakovVladimir <kaktusation@gmil.com>
2022-03-19 02:57:13 +0300

    Зафиксированы результаты, построены графики

--- Термодинамика/Диффузия/plt1.py
+++ Термодинамика/Диффузия/plt1.py
@@ -1,20 +1,29 @@
+from cProfile import label
 import laba_functions as lf
 import matplotlib.pyplot as plt
 import math
 import csv 
 from scipy.interpolate import interp1d
-def countD(voltage: list,time: list)->lf.value:
+def countD(voltage: list,time: list)->tuple:
     logVoltage = [math.log(i) for i in voltage]
-    deltaLogVoltage = [logVoltage[0]-logVoltage[i] for i in range(len(logVoltage))]
-    a,_ = lf.MNK(time,deltaLogVoltage)
-    a.print("A deltaLog")
+    a,_ = lf.MNK(time,logVoltage)
+    processTime = lf.value(-1/a.value,abs(a.error/(a.value**2)))
     V = lf.value(1200,30)
     LbyS = lf.value(5.5,0.5)
-    DValue = a.value*V.value*LbyS.value/2
+    DValue = V.value*LbyS.value/(2*processTime.value)
     DerrLbyS = abs(LbyS.error*DValue/LbyS.value)
     DerrV = abs(V.error*DValue/V.value)
-    DerrA = abs(a.error*DValue/a.value)
-    return lf.value(DValue/(100**2),math.sqrt(DerrA**2+DerrV**2+DerrLbyS**2)/(100**2))
+    DerrT = abs(processTime.error*DValue/processTime.value)
+    return lf.value(DValue/(100**2),math.sqrt(DerrT**2+DerrV**2+DerrLbyS**2)/(100**2)),processTime
+def countLength(D: lf.value)->lf.value:
+    HeMolar = 0.004
+    T = lf.value(20+273,1)
+    vAverageVal = math.sqrt(8*lf.const.R*T.value/(math.pi*HeMolar))
+    vAverageErr = T.error*0.5*vAverageVal/T.value
+    vAverage = lf.value(vAverageVal,vAverageErr)
+    length = lf.value(3*D.value/vAverage.value,
+    math.sqrt((3*D.error/vAverage.value)**2+(3*D.value*vAverage.error/(vAverage.value**2))**2))
+    return length
 def readData(path :str):
     file = open(path)
     data = csv.DictReader(file)
@@ -35,25 +44,46 @@ for i in range(5):
     v.pop(0)
     while t.pop(0)<5:   v.pop(0)
     a,b = count(voltage,time)
-    D = countD(voltage,time)
+    D, processTime = countD(voltage,time)
     Diffusion.append(D)
+    length = countLength(D)
+    print()
     print()
     print("Experiment: "+str(i+1))
-    D.print('Diffuzion coefficient')
     a.print("coefficient a")
     b.print("coefficient b")
+    print()
+    D.print('Diffuzion coefficient')
+    processTime.print("process time")
+    print()
+    length.print("Freedom dist")
+    print()
+    print("_____________________________________________________________")
     plt.yscale('log')
     plt.errorbar(x = t,
                  y = v,
-                 color = colors[i])
+                 color = colors[i],
+                 label = 'Давление '+str(40+i*40)+ ' [торр]')
 xT = [i*10 for i in range(0,75,5)]
+yT = [math.log(i/10000) for i in range(1,1000,10)]
+plt.xlabel("время [с]")
+plt.ylabel("Логарифм напряжения [log(мв)]")
 plt.xticks(xT)
+plt.yticks(yT)
+plt.legend()
 plt.grid()
 plt.show()
 plt.figure()
 aLine,bLine = lf.MNK(Pressure,Diffusion)
 xLine = [i/1000 for i in range(math.floor(Pressure[-1]*1000),math.ceil(Pressure[0]*1000+100))]
 yLine = [aLine.value*x+bLine.value for x in xLine]
+xTline = [i/50000 for i in range(0,30)]
+yTline = [i/10000 for i in range(0,50)]
+plt.xticks(xTline)
+plt.yticks(yTline)
+plt.ylabel("Коэффициент диффузии [м^2/c]")
+plt.xlabel('Обратное давление [Па^-1]')
+plt.grid()
 plt.errorbar(x = Pressure,
              y = [d.value for d in Diffusion],
              xerr = [0 for _ in Pressure],
