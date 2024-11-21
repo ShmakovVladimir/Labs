6b1c286193a1a747a4260d0500b54fde5c2673fe
Vladimir <shmakov.ve@phystech.edu>
2024-11-21 12:58:33 +0300

    fasd

--- LaTeX_Template/latex_template.sh
+++ LaTeX_Template/latex_template.sh
@@ -3,5 +3,5 @@ default_name="Очень интересная работа"
 template_path="/home/vladimir/BOTAY!/Labs/LaTeX_Template/template/template.tex"
 now_path="$(pwd)/$1"
 read -p "Текст: " name
-cp $template_path $now_path
-sed -i "s/$default_name/$name/g" $now_path
+cp "$template_path" "$now_path"
+sed -i "s/$default_name/$name/g" "$now_path"
